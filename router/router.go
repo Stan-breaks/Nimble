42d3a74bc760c03ce3613eaf3c57cc422a512726
Stan-breaks <stanleymwendwa03@gmail.com>
2026-03-10 12:41:12 +0300

    feat: router

--- router/router.go
+++ router/router.go
@@ -9,14 +9,14 @@ import (
 
 type Router struct {
 	queries   *database.Queries
-	jwtSercet []byte
+	jwtSecret []byte
 	mux       *http.ServeMux
 }
 
-func NewRouter(queries *database.Queries, jwtSercet []byte) *Router {
+func NewRouter(queries *database.Queries, jwtSecret []byte) *Router {
 	r := &Router{
 		queries:   queries,
-		jwtSercet: jwtSercet,
+		jwtSecret: jwtSecret,
 		mux:       http.NewServeMux(),
 	}
 	r.setupRoutes()
@@ -24,22 +24,14 @@ func NewRouter(queries *database.Queries, jwtSercet []byte) *Router {
 }
 
 func (r *Router) setupRoutes() {
-	// serving static files
-	fs := http.FileServer(http.Dir("public"))
-	r.mux.Handle("/static/", http.StripPrefix("/static/", middleware.AddContentType(fs)))
+	// Auth
+	authHandler := apis.NewAuthApi(r.queries, r.jwtSecret)
+	r.mux.HandleFunc("/api/register", authHandler.Register)
+	r.mux.HandleFunc("/api/login", authHandler.Login)
 
-// apis section
-	authHander := apis.NewAuthApi(r.queries, r.jwtSercet)
-	r.mux.HandleFunc("/api/login", authHander.Login)
-	r.mux.HandleFunc("/api/register", authHander.Reqister)
-	// serving protected apis
+	// Protected
 	userHandler := apis.NewUserApi(r.queries)
-	r.mux.HandleFunc("/api/me", middleware.CheckAuth(userHandler.GetCurrentUSer, r.jwtSercet))
-	dashHandler := apis.NewDashApi(r.queries)
-	r.mux.HandleFunc("/api/coordinator/students", middleware.CheckAuth(dashHandler.GetAllStudents, r.jwtSercet))
-	r.mux.HandleFunc("/api/coordinator/projects", middleware.CheckAuth(dashHandler.GetProjectsData, r.jwtSercet))
-	r.mux.HandleFunc("/api/coordinator/supervisors", middleware.CheckAuth(dashHandler.GetAllSupervisors, r.jwtSercet))
-	r.mux.HandleFunc("/api/coordinator/assign", middleware.CheckAuth(dashHandler.AssignSupervisor, r.jwtSercet))
+	r.mux.HandleFunc("/api/me", middleware.CheckAuth(userHandler.GetCurrentUser, r.jwtSecret))
 }
 
 func (r *Router) Handler() http.Handler {
